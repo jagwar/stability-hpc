aeed052f57acb47524140959dfe501b8bd0b9f09
Guillaume Salou <g.salou@gmail.com>
2023-02-27 21:49:32 +0100

    Update aws-gpu-reduce-clock.sh

--- SageMaker/custom-scripts-and-configs/aws-gpu-reduce-clock.sh
+++ SageMaker/custom-scripts-and-configs/aws-gpu-reduce-clock.sh
@@ -5,24 +5,11 @@ if [ ! nvidia-smi ]; then
    exit 0
 fi
 
-sudo nvidia-smi -pm 1
-sudo nvidia-smi --auto-boost-default=0
-
 GPUNAME=$(nvidia-smi -L | head -n1)
 echo $GPUNAME
 
-if [[ $GPUNAME == *"A100-SXM4-40GB"* ]]; then
-   nvidia-smi -ac 1215,1410
-elif [[ $GPUNAME == *"A100-SXM4-80GB"* ]]; then
-   nvidia-smi -ac 1593,1410
-elif [[ $GPUNAME == *"V100"* ]]; then
-   nvidia-smi -ac 877,1530
-elif [[ $GPUNAME == *"K80"* ]]; then
-   nvidia-smi -ac 2505,875
-elif [[ $GPUNAME == *"T4"* ]]; then
-   nvidia-smi -ac 5001,1590
-elif [[ $GPUNAME == *"M60"* ]]; then
-   nvidia-smi -ac 2505,1177
+if [[ $GPUNAME == *"A100-SXM4-80GB"* ]]; then
+   nvidia-smi -lgc 210,1305
 else
    echo "unsupported gpu"
 fi
